89cec2662b5e08ebc038a3de4d1a7662381cd389
SnowWarri0r <gerrytranCHINA@gmail.com>
2022-09-08 23:24:51 +0800

    将登录页改为侧边页登录

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@
  * @GithubUser: SnowWarri0r
  * @Date: 2022-09-07 11:10:04
  * @Company: ncuhome
- * @LastEditTime: 2022-09-08 18:35:10
+ * @LastEditTime: 2022-09-08 22:58:36
  * @FilePath: \notelog_fe\src\pages\SignIn\index.tsx
  * @Description:
  */
@@ -14,10 +14,10 @@ import {
   Avatar,
   Box,
   Button,
-  Container,
   CssBaseline,
   Grid,
   Link,
+  Paper,
   TextField,
   Typography,
 } from "@mui/material";
@@ -32,94 +32,108 @@ export default function SignIn() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
     <>
-      <CssBaseline />
       <Header />
-      <Container component="main" maxWidth="xs">
-        <Box
+      <Grid container component="main" sx={{ height: "95vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
           sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
-            component="form"
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{ mt: 3 }}
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  {...register("username", {
-                    required: "no username is dame",
-                    minLength: { value: 6, message: "too short" },
-                    maxLength: { value: 10, message: "too long" },
-                  })}
-                  helperText={errors.username ? errors.username.message : ""}
-                  error={Boolean(errors.username)}
-                  fullWidth
-                  id="username"
-                  label="User Name"
-                  name="username"
-                  autoComplete="username"
-                  variant="standard"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  {...register("password", {
-                    required: "no password is dame",
-                    pattern: {
-                      value:
-                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9~!@#$%^&*]{8,16}$/,
-                      message:
-                        "必须包含大小写字母和数字的组合，可以使用特殊字符(~!@#$%^&*)，长度在8-16之间",
-                    },
-                  })}
-                  helperText={errors.password ? errors.password.message : ""}
-                  error={Boolean(errors.password)}
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  variant="standard"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 1 }}
             >
-              Sign In
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  New User? Sign Up
-                </Link>
+              <TextField
+                {...register("username", {
+                  required: "no username is dame",
+                  minLength: { value: 6, message: "too short" },
+                  maxLength: { value: 10, message: "too long" },
+                })}
+                helperText={errors.username ? errors.username.message : ""}
+                error={Boolean(errors.username)}
+                margin="normal"
+                fullWidth
+                id="username"
+                label="User Name"
+                name="username"
+                autoComplete="username"
+                variant="standard"
+              />
+              <TextField
+                {...register("password", {
+                  required: "no password is dame",
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9~!@#$%^&*]{8,16}$/,
+                    message:
+                      "必须包含大小写字母和数字的组合，可以使用特殊字符(~!@#$%^&*)，长度在8-16之间",
+                  },
+                })}
+                helperText={errors.password ? errors.password.message : ""}
+                error={Boolean(errors.password)}
+                margin="normal"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                variant="standard"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    New User? Sign Up
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+              <Footer sx={{ mt: 5 }} />
+            </Box>
           </Box>
-        </Box>
-      </Container>
-      <Footer sx={{ mt: 5 }} />
+        </Grid>
+      </Grid>
     </>
   );
 }
